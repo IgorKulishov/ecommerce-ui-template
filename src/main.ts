4a0a4abe8b5cc5e15b71db8b0c2d144e55ca9d1a
igor.k <igor.kulishov@adesa.com>
2019-08-07 01:03:55 -0400

    updated skin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register(`${window.location.origin}/ngsw-worker.js`);
+  });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
