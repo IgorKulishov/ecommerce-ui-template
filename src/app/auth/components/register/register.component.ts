e0b8335a7209753a3e0234334928e39c049dc463
Igor Kulishov <igorkulishov@Igors-MacBook-Pro.local>
2019-11-06 21:37:30 -0500

    translation

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,10 +3,11 @@ import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { getAuthSelector, registerUserMapper } from '../../store/select/auth.selectors';
+import {getAuthSelector, loginErrorMapper, registerUserMapper} from '../../store/select/auth.selectors';
 import { AppStates } from '../../../app.states';
 import { LoginService } from '../../../core/services/login.service';
 import { RegisterUserAction } from '../../store/actions/login.actions';
+import {ErrorMessage} from '../login/login.component';
 
 @Component({
   templateUrl: './register.component.html',
@@ -15,6 +16,8 @@ import { RegisterUserAction } from '../../store/actions/login.actions';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
+  errorMessage: ErrorMessage;
+  loading = false;
 
   constructor(private router: Router,
               private loginService: LoginService,
@@ -37,6 +40,17 @@ export class RegisterComponent implements OnInit {
         mobile:   [null, [Validators.required, Validators.minLength(9), Validators.maxLength(10), Validators.pattern('^[0-9]+$')]],
         password: [null, [Validators.required, Validators.minLength(6)]],
         confirmPassword: [null, [Validators.required, Validators.minLength(6)]]
+      });
+
+      // Error handing
+      this.store.select(getAuthSelector).pipe(
+        map(loginErrorMapper)
+      )
+      .subscribe((error: ErrorMessage) => {
+        if ( error && error.message ) {
+          this.errorMessage = error;
+          this.loading = false;
+        }
       });
     }
 
