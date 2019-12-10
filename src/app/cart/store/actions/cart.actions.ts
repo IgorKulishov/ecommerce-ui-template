fa4b387fc52f5c3c101e75084e2fe454583f0ef6
Igor Kulishov <igorkulishov@Igors-MacBook-Pro.local>
2019-12-10 00:28:58 -0500

    processed order

--- src/app/cart/store/actions/cart.actions.ts
+++ src/app/cart/store/actions/cart.actions.ts
@@ -8,9 +8,11 @@ export const SAVE_CURRENT_ORDER_IN_STORE = 'SAVE_CURRENT_ORDER_IN_STORE';
 export const GET_CURRENT_ORDER_FROM_STORE = 'GET_CURRENT_ORDER_FROM_STORE';
 export const GET_CURRENT_ORDER_FROM_STORE_SUCCESS = 'GET_CURRENT_ORDER_FROM_STORE_SUCCESS';
 export const CHECK_OUT = 'CHECK_OUT';
+export const GET_PROCESSED_ORDER_FROM_STORE = 'GET_PROCESSED_ORDER_FROM_STORE';
 export const CHECK_OUT_SUCCESS = 'CHECK_OUT_SUCCESS';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const SAVE_PLACED_ORDER = 'SAVE_PLACED_ORDER';
+export const SAVE_PLACED_ORDER_DETAILS = 'SAVE_PLACED_ORDER_DETAILS';
 
 export class AddToCart implements Action {
   readonly type = ADD_TO_CART;
@@ -35,6 +37,11 @@ export class GetCurrentOrderFromStore implements Action {
   constructor(public payload?: any) {
   }
 }
+export class GetProcessedOrderFromStore implements Action {
+  readonly type = GET_PROCESSED_ORDER_FROM_STORE;
+  constructor(public payload?: any) {
+  }
+}
 export class GetCurrentOrderFromStoreSuccess implements Action {
   readonly type = GET_CURRENT_ORDER_FROM_STORE_SUCCESS;
   constructor(public payload?: any) {
@@ -53,5 +60,10 @@ export class CheckOutSuccess implements Action {
 export class SavePlacedOrder implements Action {
   readonly type = SAVE_PLACED_ORDER;
   constructor(public payload: UserDetails ) {
+  }
+}
+export class StorePlacedOrderDetails implements Action {
+  readonly type = SAVE_PLACED_ORDER_DETAILS;
+  constructor(public payload: any ) {
   }
 }
