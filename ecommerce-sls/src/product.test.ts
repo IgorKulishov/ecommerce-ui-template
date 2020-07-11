f8d1051befaa2d8c8484f2d209e193aff0471457
Vasiliy Malakhov <mvamgc@gmail.com>
2020-07-10 21:53:46 -0400

    Product service mock: search by name

--- ecommerce-sls/src/product.test.ts
+++ ecommerce-sls/src/product.test.ts
@@ -1,7 +1,29 @@
 import * as product from './product';
 
-test('correct status code async', async() => {
-  let data = await product.search({queryStringParameters: {}}, {});
+test('search non existing product', async() => {
+  let data = await product.search({queryStringParameters: {name: 'not there'}}, {});
   expect(data).not.toBeNull();
   expect(data.statusCode).toBe(200);
+  expect(data.body).toBe('[]');
+});
+
+
+test('search with single result', async() => {
+  let data = await product.search({queryStringParameters: {name: 'ink'}}, {});
+  expect(data).not.toBeNull();
+  expect(data.statusCode).toBe(200);
+  expect(JSON.parse(data.body).length).toBe(1);
+  expect(JSON.parse(data.body)[0].name).toBe('Magic Ink');
+  expect(JSON.parse(data.body)[0].id).toBe(2);
+});
+
+test('search with two results', async() => {
+  let data = await product.search({queryStringParameters: {name: 'magic'}}, {});
+  expect(data).not.toBeNull();
+  expect(data.statusCode).toBe(200);
+  expect(JSON.parse(data.body).length).toBe(2);
+  expect(JSON.parse(data.body)[0].name).toBe('Magic Pen');
+  expect(JSON.parse(data.body)[0].id).toBe(1);
+  expect(JSON.parse(data.body)[1].name).toBe('Magic Ink');
+  expect(JSON.parse(data.body)[1].id).toBe(2);
 });
--- ecommerce-sls/src/product.ts
+++ ecommerce-sls/src/product.ts
@@ -1,14 +1,37 @@
 import {APIGatewayProxyResult} from 'aws-lambda';
 import 'source-map-support/register';
+import {ProductDao} from "./product.dao";
+
+let products: ProductDao[] = [];
+
+products.push({
+  id: 1,
+  name: 'Magic Pen',
+  slug: 'magic-pen'
+});
+
+products.push({
+  id: 2,
+  name: 'Magic Ink',
+  slug: 'magic-ink'
+});
+
 
 // export const search: APIGatewayProxyHandler = (event, _context): APIGatewayProxyResult => {
 // export const search = async (event, _context): Promise<any> => {
 export async function search(event, _context): Promise<APIGatewayProxyResult> {
+  let name = event.queryStringParameters['name'];
+
+  let found: ProductDao[] = [];
+  for(let i=0; i<products.length; i++) {
+    if(products[i].name.toLowerCase().indexOf(name.toLowerCase()) !== -1) {
+      found.push(products[i]);
+    }
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-        msg: 'ok',
-        input: event.queryStringParameters
-    }),
+    body: JSON.stringify(found),
   };
 }
+
