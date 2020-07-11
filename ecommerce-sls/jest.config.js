cfe019f28daa81682551881badf49fcf5e0325b0
Vasiliy Malakhov <mvamgc@gmail.com>
2020-07-11 11:54:42 -0400

    Test coverage

--- ecommerce-sls/jest.config.js
+++ ecommerce-sls/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  coverageProvider: "v8",
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
 };
