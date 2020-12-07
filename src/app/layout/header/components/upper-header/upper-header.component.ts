eafcca76ce331a4659371a5334cdfb193b859e34
Andrey M <finix@list.ru>
2020-12-07 21:28:35 +0500

    moved lang & logout links outside user's menu

--- src/app/layout/header/components/upper-header/upper-header.component.ts
+++ src/app/layout/header/components/upper-header/upper-header.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {TranslateService} from '@ngx-translate/core';
+import { AppStates } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/app.states';
+import { SelectLanguageAction } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/auth/store/actions/login.actions';
+
+import { languages} from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/core/consts/consts';
+import { SessionService } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/core/services/session.service';
 
 @Component({
   selector: 'app-upper-header',
   templateUrl: './upper-header.component.html',
   styleUrls: ['./upper-header.component.scss']
 })
-export class UpperHeaderComponent {
-  constructor() {
+export class UpperHeaderComponent implements OnInit{
+  languages: string[] = languages;
+  selectedLanguage = 'en';
+
+  constructor(private sessionService: SessionService,
+              private store: Store<AppStates>,
+              private translateService: TranslateService) {
+  }
+
+  ngOnInit(){}
+
+  logout() {
+    this.sessionService.logout();
+  }
+  onLanguageSelect(lang) {
+    this.store.dispatch(new SelectLanguageAction(lang));
   }
 }
