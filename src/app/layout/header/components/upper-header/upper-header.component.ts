19d64856765692d7305808fc84f8a6cfdfe4e2e7
Igor Kulishov <igorkulishov@Igors-MacBook-Pro.local>
2020-12-08 01:33:49 -0500

    dev/EC-27: update import path

--- src/app/layout/header/components/upper-header/upper-header.component.ts
+++ src/app/layout/header/components/upper-header/upper-header.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {TranslateService} from '@ngx-translate/core';
-import { AppStates } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/app.states';
-import { SelectLanguageAction } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/auth/store/actions/login.actions';
+import { AppStates } from '../../../../app.states';
+import { SelectLanguageAction } from '../../../../auth/store/actions/login.actions';
 
-import { languages} from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/core/consts/consts';
-import { SessionService } from 'd:/Andrey/prog/Andrew/project_igor/ecommerce-ui-template/src/app/core/services/session.service';
+import { languages} from '../../../../core/consts/consts';
+import { SessionService } from '../../../../core/services/session.service';
 
 @Component({
   selector: 'app-upper-header',
