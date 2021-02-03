2c3111a39172faa1ba02611266b64ed8dfc6f14f
Andrey M <finix@list.ru>
2021-02-03 07:39:21 +0500

    added unit tests to increse coverage

--- src/app/products/components/child-components/add-to-cart/add-to-cart.component.spec.ts
+++ src/app/products/components/child-components/add-to-cart/add-to-cart.component.spec.ts
@@ -64,4 +64,90 @@ describe('AddToCartComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should increase quantity +1', () => {
+    component.quantity = 1
+    component.maxQuantity = 10
+    component.incrementQuantity()
+    expect(component.quantity).toEqual(2)
+  });
+
+  it('should not change quantity', () => {
+    component.quantity = 3
+    component.maxQuantity = 0
+    component.incrementQuantity()
+    expect(component.quantity).toEqual(3)
+  });
+
+  it('should decrease quantity -1', () => {
+    component.quantity = 3
+    component.minQuantity = 1
+    component.decrementQuantity()
+    expect(component.quantity).toEqual(2)
+  });
+
+  it('should not change quantity', () => {
+    component.quantity = 2
+    component.minQuantity = 10
+    component.decrementQuantity()
+    expect(component.quantity).toEqual(2)
+  });
+
+  it('should show a warning message if entered number is not a number', () => {
+    let numberEntered = 'abc'
+    component.checkIfExceedLimit(numberEntered)
+    expect(component.limitStockMessage).toEqual('Please enter a number')
+  });
+
+  it('should the quantity to equal maxQuantity if entered number >= maxQuantity', () => {
+    let numberEntered = 13
+    component.maxQuantity = 10
+    component.checkIfExceedLimit(numberEntered)
+    expect(component.quantity).toEqual(10)
+  });
+
+  it('should the quantity to equal minQuantity if entered number < minQuantity', () => {
+    let numberEntered = 0
+    component.minQuantity = 1
+    component.checkIfExceedLimit(numberEntered)
+    expect(component.quantity).toEqual(1)
+  });
+
+  it('should the quantity to equal entered number', () => {
+    let numberEntered = 5
+    component.minQuantity = 1
+    component.maxQuantity = 10
+    component.checkIfExceedLimit(numberEntered)
+    expect(component.quantity).toEqual(5)
+  });
+
+  it('should call startIntervalIcrementQnty func if input param = incrementQuantity', () => {
+    let changeQuantity = 'incrementQuantity'
+    spyOn(component, 'startIntervalIcrementQnty')
+    component.mousedown(changeQuantity)
+    expect(component.startIntervalIcrementQnty).toHaveBeenCalled()
+  });
+
+  it('should call startIntervalDecrementQnty func if input param = decrementQuantity', () => {
+    let changeQuantity = 'decrementQuantity'
+    spyOn(component, 'startIntervalDecrementQnty')
+    component.mousedown(changeQuantity)
+    expect(component.startIntervalDecrementQnty).toHaveBeenCalled()
+  });
+
+  it('should call clearIntervalIcrementQnty func if input param = incrementQuantity', () => {
+    let changeQuantity = 'incrementQuantity'
+    spyOn(component, 'clearIntervalIcrementQnty')
+    component.mouseup(changeQuantity)
+    expect(component.clearIntervalIcrementQnty).toHaveBeenCalled()
+  });
+
+  it('should call clearIntervalDecrementQnty func if input param = decrementQuantity', () => {
+    let changeQuantity = 'decrementQuantity'
+    spyOn(component, 'clearIntervalDecrementQnty')
+    component.mouseup(changeQuantity)
+    expect(component.clearIntervalDecrementQnty).toHaveBeenCalled()
+  });
+  
+  
 });
