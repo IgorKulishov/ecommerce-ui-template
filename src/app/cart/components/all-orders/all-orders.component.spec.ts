10e5a18823d278a2a194be036284c282962e652d
Andrey <finix@list.ru>
2021-04-11 08:25:35 +0500

    added dependencies in test

--- src/app/cart/components/all-orders/all-orders.component.spec.ts
+++ src/app/cart/components/all-orders/all-orders.component.spec.ts
@@ -1,16 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Store } from "@ngrx/store";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { cartReducerStateMock } from "../../../../test/mock";
+import { CartState } from "../../store/states/cart.states";
 
-import { AllOrdersComponent } from './all-orders.component';
+import { AllOrdersComponent } from "./all-orders.component";
 
-describe('AllOrdersComponent', () => {
+describe("AllOrdersComponent", () => {
   let component: AllOrdersComponent;
   let fixture: ComponentFixture<AllOrdersComponent>;
+  let store: MockStore<CartState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AllOrdersComponent ]
-    })
-    .compileComponents();
+      declarations: [AllOrdersComponent],
+      providers: [provideMockStore({ initialState: cartReducerStateMock })],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +24,7 @@ describe('AllOrdersComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
